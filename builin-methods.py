9d7c7f89fc2e9d9944e838f78d96309e803e7f5b
mohamad <mohamad97mj@gmail.com>
2020-08-22 18:53:24 +0430

    predefined methods to overwrite

---
+++ builin-methods.py
@@ -0,0 +1,19 @@
+class MyNumber:
+
+    def __init__(self):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+    def __gt__(self, other):
+        pass
+
+    def __lt__(self, other):
+        pass
+
+    def __le__(self, other):
+        pass
+
+    def __ge__(self, other):
+        pass
