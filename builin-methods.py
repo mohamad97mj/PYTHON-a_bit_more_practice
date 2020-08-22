e82ad4eab46c6b82f87376fe0e9bb9f00f8442f7
mohamad <mohamad97mj@gmail.com>
2020-08-22 19:42:11 +0430

    lamda and built in methods updated

--- builin-methods.py
+++ builin-methods.py
@@ -1,19 +1,56 @@
-class MyNumber:
+class Currency:
 
-    def __init__(self):
-        pass
+    def __init__(self, code, exchange_to_usd):
+        self.amount = 0.00
+        self.code = code
+        self.exchange_to_usd = exchange_to_usd
+
+    def set_amount(self, amount):
+        self.amount = amount
+
+    def in_currency(self, amount):
+        return amount / self.exchange_to_usd
+
+    def to_usd(self, amount=None):
+        to_convert = amount or self.amount
+        return to_convert * self.exchange_to_usd
 
     def __eq__(self, other):
-        pass
+        return self.to_usd() == other.to_usd()
 
     def __gt__(self, other):
-        pass
+        return self.to_usd() > other.to_usd()
 
     def __lt__(self, other):
-        pass
+        return self.to_usd() < other.to_usd()
 
     def __le__(self, other):
-        pass
+        return self.to_usd() <= other.to_usd()
 
     def __ge__(self, other):
-        pass
+        return self.to_usd() >= other.to_usd()
+
+
+def _get_currency_resource(resource, transform=(lambda x: x)):
+    data = {
+        'items': [
+            {'code': 'usd', 'amount_to_usd': 1.00},
+            {'code': 'usd', 'amount_to_usd': 1.21},
+            {'code': 'usd', 'amount_to_usd': 1.07},
+            {'code': 'usd', 'amount_to_usd': 0.14},
+        ]
+    }
+    my_resource = data[resource]
+    return [transform(x) for x in my_resource]
+
+
+def get_currency_codes():
+    return _get_currency_resource("items", lambda x: x['code'].upper)
+
+
+def get_currencies():
+    return _get_currency_resource("items", lambda x: Currency(x['code'], x['amount_to_usd']))
+
+
+print(get_currency_codes())
+print(get_currencies())
--- lambdas.py
+++ lambdas.py
@@ -1,5 +1,6 @@
 def alter(values, check):
-    return [val for val in values if check(val)]
+    return list(filter(check, values))
+    # return [val for val in values if check(val)]
 
 
 my_list = [1, 2, 3, 4, 5]
@@ -13,3 +14,15 @@ def check_not_five(x):
 
 another_list = alter(my_list, check_not_five)
 print(another_list)
+
+
+def remove_numbers(value):
+    return alter(value, lambda x: x not in [str(n) for n in range(10)])
+
+
+def skip_letter(value, letter):
+    return alter(value, lambda x: x != letter)
+
+
+print(remove_numbers("hel5lo"))
+print(skip_letter("hello", 'e'))
